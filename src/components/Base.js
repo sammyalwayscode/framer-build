aa643f88bab5b61002406b72c868aa9ad71ae820
sammyalwayscode <olorundasamuel2@gmail.com>
2021-11-30 15:26:29 +0100

    framer-tell

---
+++ src/components/Base.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerBase = {
+  hidden: {
+    x: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      type: "spring",
+    },
+  },
+};
+
+const NextContainer = {
+  hidden: {
+    x: "-100vw",
+  },
+
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
+
+const Base = ({ addBase, pizza }) => {
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+  return (
+    <motion.div
+      className="base container"
+      variants={containerBase}
+      initial="hidden"
+      animate="visible"
+    >
+      <h3>Step 1: Choose Your Base</h3>
+      <ul>
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
+          return (
+            <motion.li
+              whileHover={{ scale: 1.3, originX: 0, color: "yellow" }}
+              transition={{ type: "spring", stiffness: 300 }}
+              key={base}
+              onClick={() => addBase(base)}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
+        })}
+      </ul>
+
+      {pizza.base && (
+        <motion.div
+          variants={NextContainer}
+          initial="hide"
+          animate="show"
+          className="next"
+        >
+          <Link to="/toppings">
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: "0px 0px 8px rgb(255,255,255)",
+                boxShadow: "0px 0px 8px rgb(255,255,255)",
+              }}
+            >
+              Next
+            </motion.button>
+          </Link>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+};
+
+export default Base;
